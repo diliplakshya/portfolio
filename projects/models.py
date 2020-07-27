5332c742680e35c83da753f828f91b1312782812
Dilip Kumar <diliplakshya@gmail.com>
2020-07-27 10:29:42 +0530

    Added boolean variable in models for projects

--- projects/models.py
+++ projects/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Project(models.Model):   
-    title = models.CharField(max_length=30)
-    description = models.TextField()
-    image = models.CharField(max_length=20)
-    technology = models.CharField(max_length=200)
-    url = models.URLField()
+    title           =   models.CharField(max_length=30)
+    description     =   models.TextField()
+    image           =   models.CharField(max_length=20)
+    technology      =   models.CharField(max_length=200)
+    url             =   models.URLField()
+    is_sc_enabled   =   models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
