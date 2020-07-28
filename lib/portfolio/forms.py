cabfa5c0170e24b5937adfc1d57ac30f11ee1369
Dilip Kumar <diliplakshya@gmail.com>
2020-07-28 19:04:22 +0530

    Added forms for email

---
+++ lib/portfolio/forms.py
@@ -0,0 +1,27 @@
+"""
+Django forms for portfolio project
+"""
+
+from django import forms
+
+
+class ContactForm(forms.Form):
+    email        =   forms.EmailField(widget=forms.TextInput(
+                                    attrs=
+                                    {
+                                        "placeholder" : "Enter your email address."
+                                    }))
+    subject         =   forms.CharField(max_length=50, 
+                                widget=forms.TextInput(
+                                    attrs=
+                                    {
+                                        "placeholder" : "Enter the Subject",
+                                        "class" : "form-control-sm",
+                                        "type" : "text"
+                                    }
+                                    ))
+    message         =   forms.CharField(
+                            widget=forms.Textarea(
+                                attrs=
+                                {"rows": 3, "cols":20, "class" : "form-control-sm", "placeholder" : "Enter the Message"}
+                                ))
